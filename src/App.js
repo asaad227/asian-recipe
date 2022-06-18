2e5f681a0844f52bef1b9a05ae3e0e13cbae10a8
asaad227 <a.saad227@yahoo.com>
2022-06-19 00:58:29 +0100

    placeholder txt update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import './App.css';
 
 function App() {
   const [dishInput, setDishInput] = useState('');
-  const [cuisinInput, setCuisinInput] = useState('');
+  const [cuisineInput, setcuisineInput] = useState('');
   const [data, setData] = useState([]);
 
  
   async function testFetch(){
    
-    const responce = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${dishInput}&q=${cuisinInput}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&random=true`);
+    const responce = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${dishInput}&q=${cuisineInput}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&random=true`);
     const data = await responce.json();
     const {hits} = data
  console.log(hits)
@@ -21,8 +21,8 @@ function App() {
   function onSubmit(e){
     e.preventDefault();
     testFetch()
-    setCuisinInput('')
-    setCuisinInput(e.target.value)
+    setcuisineInput('')
+    setcuisineInput(e.target.value)
     
   } 
  
@@ -33,16 +33,16 @@ function App() {
   return (
     <div className="App">
      <form onSubmit ={onSubmit} className='form'>
-    <input className='textIn'  onChange={(e)=> {setDishInput(e.target.value)}} type='text' value={dishInput} placeholder='Write here then select cuisin type'/>
-    <input id='home' className='cuisinInput' type='button' onClick={()=> window.location.reload()} value='Home' on='true'/>
-   <input id='indian' className='cuisinInput' type='button' onClick={onSubmit} value='Indian' on='true'/>
-   <input id='chinese' className='cuisinInput' type='button' onClick={onSubmit} value='Chinese' on='true'/>
-   <input id='korean' className='cuisinInput' type='button' onClick={onSubmit} value='Korean' on='true'/>
-   <input id='asian' className='cuisinInput' type='button' onClick={onSubmit} value='Asian' on='true'/>
+    <input className='textIn'  onChange={(e)=> {setDishInput(e.target.value)}} type='text' value={dishInput} placeholder='Type main ingredients then select your CUISINE'/>
+    <input id='home' className='cuisineInput' type='button' onClick={()=> window.location.reload()} value='Home' on='true'/>
+   <input id='indian' className='cuisineInput' type='button' onClick={onSubmit} value='Indian' on='true'/>
+   <input id='chinese' className='cuisineInput' type='button' onClick={onSubmit} value='Chinese' on='true'/>
+   <input id='korean' className='cuisineInput' type='button' onClick={onSubmit} value='Korean' on='true'/>
+   <input id='asian' className='cuisineInput' type='button' onClick={onSubmit} value='Asian' on='true'/>
    
 
 
-       {/* <input onChange={(e)=> {setCuisinInput(e.target.value)}} type='text' placeholder='Meat or Chicken'/> */}
+       {/* <input onChange={(e)=> {setcuisineInput(e.target.value)}} type='text' placeholder='Meat or Chicken'/> */}
        {/* <button className='submit'>Submit</button> */}
      </form>
      
