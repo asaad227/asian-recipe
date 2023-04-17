f3075b269b73170876a7146ba18402c77b31b1f2
asaad227 <a.saad227@yahoo.com>
2023-04-17 22:19:11 +0100

    update css

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import Main from './Components/Main/Main';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>
-  </BrowserRouter>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
