8a15cc8be586ec646f131b630bde482f677f9011
asaad227 <a.saad227@yahoo.com>
2023-04-17 21:57:25 +0100

    update showingredient function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react'
-import Pic from './asset/Recipe-site.png'
+import React, { useEffect, useState} from 'react'
 import './App.css';
 
 
@@ -8,15 +7,25 @@ function App() {
   const [dishInput, setDishInput] = useState('');
   const [cuisinInput, setCuisinInput] = useState('');
   const [data, setData] = useState([]);
+  const[fav, setFav] = useState([])
+  const [show, setShow] = useState([])
+  const[local, setLocal] = useState(JSON.parse(localStorage.getItem("myRecipe")))
+useEffect(()=>{
+setLocal(local)
+},[local])
 
  
   async function testFetch(){
    
     const responce = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${dishInput}&q=${cuisinInput}&app_id=${process.env.REACT_APP_API_ID}&app_key=${process.env.REACT_APP_API_KEY}&random=true`);
     const data = await responce.json();
-    const {hits} = data
- console.log(hits)
-    setData(hits);
+    const {hits} = data;
+
+    const exist = hits.map((e, i)=> e? {...e, id:1+i}:e)
+      const listItem = localStorage.setItem("myRecipe", JSON.stringify(exist))
+     setData(listItem)
+    
+  
       }
     //  testFetch()
   function onSubmit(e){
@@ -26,10 +35,48 @@ function App() {
     setCuisinInput(e.target.value)
     
   } 
- 
- 
+
+
+ function addFav(index){
+  const list = [...local];
+list[index].id = !list[index].id;
+
+  const finalList = list.filter(e=> !e.id);
+  console.log(finalList);
+  const final = localStorage.setItem("myFav", JSON.stringify(finalList))
+  setFav(final)
+ }
+const showIngredient = (id)=>{
+  setShow((preventState)=>({
+    ...preventState, [id]:!preventState[id]
+  }))
   
+}
  
+ function checklist(){
+  if(local === null|| local.length === 0){
+    return(<div>
+      <p>Please fetch the data</p>
+    </div>)
+  }else{
+   return (local.map((e, index)=> <section className='flex-box' key={index}><h4 className='label'>{e.recipe.label}</h4>
+    <div>
+    <img src={e.recipe.image} width={200} height={200} alt={e.recipe.label}/>
+    </div>
+    
+  <button onClick={()=> showIngredient(index)}>{show[index]? "Hide":"Show"}</button>
+   <p className="ingredientPara" >{show[index]? e.recipe.ingredients.map((x,i)=>{
+return(<div>
+  <ul key={i}><li>{x.text}</li></ul>
+  </div>)
+   })
+:""}</p>
+    
+    <button type='submit' key={e.id} onClick={()=>addFav(index)}>Add</button>
+    </section>))
+  }
+ }
+  console.log(local);
  
   return (
     <div className="App">
@@ -40,23 +87,14 @@ function App() {
    <input id='korean' className='cuisinInput' type='button' onClick={onSubmit} value='Korean' on='true'/>
    <input id='asian' className='cuisinInput' type='button' onClick={onSubmit} value='Asian' on='true'/>
    <input id='asian' className='cuisinInput' type='button' onClick={()=> window.location.reload()} value='Home' on='true'/>
-
-
-       {/* <input onChange={(e)=> {setCuisinInput(e.target.value)}} type='text' placeholder='Meat or Chicken'/> */}
-       {/* <button className='submit'>Submit</button> */}
      </form>
      
  <div className='flex-container'>
-{data.map((e, index)=> <section className='flex-box' key={index}><h4 className='label'>{e.recipe.label}</h4>
-<div>
-<img src={e.recipe.image} width={200} height={200} alt={e.recipe.label}/>
+ 
+{checklist()}
 </div>
-<p className='ingredientPara'>{e.recipe.ingredientLines}</p>
-</section>)}
 </div>
 
-    </div>
-  );
-}
+)}
 
 export default App;
