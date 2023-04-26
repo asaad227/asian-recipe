65a741d83cd549360794d7480cf82656e3838fb8
Ahammed <66068025+asaad227@users.noreply.github.com>
2023-04-26 18:10:08 +0100

    update nav icons

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import "./index.css";
 import {Link} from 'react-router-dom';
-import { RiHomeSmileLine } from "react-icons/ri";
-import {  MdOutlineFoodBank  } from "react-icons/md";
 export default function Nav() {
   return (
     <div>
         <button className='navBtn'><Link className='navList' to="/">
-        <RiHomeSmileLine  className='iconNav'/>
+        <i className="fa fa-home" aria-hidden="true"></i>
           </Link></button>
-         
+
+          <button className='navBtn'><Link to="/recipe">
+          <i className="fa fa-cutlery" aria-hidden="true"></i>
+          </Link></button>
+
           <button className='navBtn'><Link to="/fav">
           <i className="fa fa-heart" aria-hidden="true"></i>
           </Link></button>
-          <button className='navBtn'><Link to="/recipe">
-          <MdOutlineFoodBank className='iconNav' />
-          </Link></button>
+         
           <button className='navBtn'><Link to="/calories">
           <i className="fa fa-calculator" aria-hidden="true"></i>
           </Link></button>
